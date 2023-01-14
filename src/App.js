173966c82007e4bf22d9e4003df8b7837063f0ea
pradipdhale <pradipdhale127@gmail.com>
2023-01-14 17:21:39 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import GetStartedPage from './Components/GetStartedPage';
 import FormContainer from './Components/FormComponent/FormContainer';
+import Footer from './Components/GetStartedPage/Partials/Footer';
 function App() {
   return (
     <>
     <GetStartedPage />
     <FormContainer />
+    <Footer />
     </>
   );
 }
--- src/Components/GetStartedPage/Partials/Footer.js
+++ src/Components/GetStartedPage/Partials/Footer.js
@@ -1,9 +1,31 @@
+import styled from '@emotion/styled'
+import { Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
 
+
+const StyledParagraph = styled(Typography)`
+   text-align: center;
+   margin-bottom: 50px;
+   background-color: #f9f9f9;
+   display: flex;
+  align-items: center;
+  width: 300px;
+`
+const FooterWrapper = styled(Box)`
+  position: absolute;
+  bottom: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+
 const Footer = () => {
   return (
-    <Box></Box>
+    <FooterWrapper>
+      <StyledParagraph>Terms of use | Privacy policy</StyledParagraph>
+    </FooterWrapper>
   )
 }
 
